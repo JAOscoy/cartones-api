80d27b3d25549bc8f3342865a2867f22b3d8d0e7
JAOscoy <ja.oscoyml@gmail.com>
2020-11-12 19:37:55 -0600

    env correct

--- config/env.sh
+++ config/env.sh
@@ -1,4 +1,4 @@
 export NODE_ENV='development'
 export PORT=3000
 export SECRET='secret' # para mayor seguridad puedes cambiar esto por el secreto de tu preferencia
-export MONGODB_URI='mongodb+srv://introabd:introabd1234@cluster0.yx5qd.mongodb.net/cartonesHidalgo'
+export MONGODB_URI='mongodb+srv://introabd:<password>@cluster0.yx5qd.mongodb.net/cartonesHidalgo'
